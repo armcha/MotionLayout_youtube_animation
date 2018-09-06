7021e759a27f4208fdf25b02f2886fad62be83bf
Arman Chatikyan <chatikyana@gmail.com>
2018-09-06 11:42:00 +0400

    Initial commit

---
+++ app/src/main/java/io/armcha/youtubeanimation/SingleTouchableMotionLayout.kt
@@ -0,0 +1,52 @@
+package io.armcha.youtubeanimation
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+
+/**
+ *
+ * Created by Arman Chatikyan on 05 Sep 2018
+ *
+ */
+
+class SingleTouchableMotionLayout(context: Context, attributeSet: AttributeSet? = null) : MotionLayout(context, attributeSet) {
+
+    private val viewToDetectTouch by lazy {
+        findViewById<View>(R.id.videoView) //TODO move to Attributes
+    }
+    private val viewRect = Rect()
+    private var touchStarted = false
+
+    init {
+        setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                touchStarted = false
+                Log.e("onTransitionCompleted ", "onTransitionCompleted $currentState")
+            }
+        })
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.actionMasked) {
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                touchStarted = false
+                return super.onTouchEvent(event)
+            }
+        }
+        Log.e("onTouchEvent ", "actionMasked ${event.actionMasked}")
+        if (!touchStarted) {
+            viewToDetectTouch.getHitRect(viewRect)
+            touchStarted = viewRect.contains(event.x.toInt(), event.y.toInt())
+        }
+        return touchStarted && super.onTouchEvent(event)
+    }
+}
---
+++ app/src/main/java/io/armcha/youtubeanimation/VideoFragment.kt
@@ -0,0 +1,23 @@
+package io.armcha.youtubeanimation
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_video.*
+
+class VideoFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_video, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
